692564e63782ad17ad6ca3b7d04c7d018360f8de
Smarsden007 <seannmarsden@gmail.com>
2022-05-10 19:19:47 -0700

    SLM

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,8 +1,42 @@
-// implement your posts router here
-
-
 const express = require('express')
-
+const Post = require('./posts-model')
 const router = express.Router()
+
+router.get('/', (req,res) => {
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be retrieved",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
+router.get('/:id', async (req,res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+router.post('/', (req,res) => {
+
+})
+router.delete('/:id', (req,res) => {
+
+})
+router.put('/:id', (req,res) => {
+
+})
+router.get('/:id/messages', (req,res) => {
+
+})
 
 module.exports = router;
